752626ddbc771223ca2c84ca7df45c05f3a143bb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-27 22:36:37 -0300

    Ta feito e funcionando perfeitamente

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string resposta = " Eu sou Groot";
+string pergunta = "";
+
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"Groot Chatbot");
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Opa, eu sou a Bianca esse é meu amigo Groot, fala com ele");
+Console.WriteLine("qunado você enjoar dele é so dizer tchau");
+Console.ResetColor();
+Console.WriteLine();
+
+while (pergunta != "TCHAU")
+{
+    Console.Write("Pergunta:   "); pergunta = Console.ReadLine()!.ToUpper();
+    if (pergunta != "tchau")
+    {
+    Console.WriteLine($"     Resposta: {resposta}.");}
+
+    else if (pergunta == "tchau")
+    {
+        Console.WriteLine($"     Resposta:{resposta}!");
+    }
+}
